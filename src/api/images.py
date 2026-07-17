cfea1ce562b64269f1051e0630ea1a8b328c6fc2
Konst_MSK <kostymoskovskiy@mail.ru>
2026-07-17 15:44:00 +0300

    Добавлена таска для работы с изображениями resize_image через библиотеку pillow. Протестирована и работает в отдельном процессе через celery

--- src/api/images.py
+++ src/api/images.py
@@ -1,6 +1,6 @@
 import shutil
 from fastapi import APIRouter, UploadFile
-
+from src.tasks.tasks import resize_image
 
 router = APIRouter(
     prefix="/images",
@@ -10,6 +10,9 @@ router = APIRouter(
 
 @router.post("")
 def upload_image(file: UploadFile):
-    with open(f"src/static/images/{file.filename}", "wb+") as new_file:
+    image_path = f"src/static/images/{file.filename}"
+    with open(image_path, "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
+
+    resize_image.delay(image_path)
 
