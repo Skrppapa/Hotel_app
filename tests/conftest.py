499f06e9ee5b3bf675dfa99062fe5864fa24b10f
Konst_MSK <kostymoskovskiy@mail.ru>
2026-07-20 21:17:14 +0300

    Добавлена фикстура создания пользователя register_user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,30 @@
 import pytest
 from src.config import settings
 from src.database import Base, engine_null_pull
+from src.main import app
 from src.models import *
+from httpx import AsyncClient, ASGITransport
 
-@pytest.fixture(scope="session", autouse=True)   # scope важный параметр, определяющий как часто будет исполняться фикстура
-async def async_main():
+
+@pytest.fixture(scope="session", autouse=True)  # scope важный параметр, определяющий как часто будет исполняться фикстура
+def check_test_mode():
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database(check_test_mode):
     async with engine_null_pull.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "test@mail.com",
+                "password": "12345"
+            }
+        )
