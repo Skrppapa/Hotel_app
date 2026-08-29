15dd0ff840508caa50aaa82d047716ca6661e852
Konst_MSK <kostymoskovskiy@mail.ru>
2026-08-29 12:20:48 +0300

    Добавлен интеграционный тест на полный путь авторизации register -> login -> me -> logout В файле src/api/auth добавлен блок try except в ручкуё регистрации

--- src/api/auth.py
+++ src/api/auth.py
@@ -28,10 +28,14 @@ async def register_user(
     db: DBDep
 ):
     """Регистрация пользователя"""
-    hashed_password = AuthService().hash_password(data.password)
-    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-    await db.users.add(new_user_data)
-    await db.commit()
+    try:
+        hashed_password = AuthService().hash_password(data.password)
+        new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+        await db.users.add(new_user_data)
+        await db.commit()
+    except:
+        raise HTTPException(status_code=400)
+
     return {"status": "OK"}
 
 
